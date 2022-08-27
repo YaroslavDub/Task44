93f4c524b98e0ef85379c914a963106c38f2bdcf
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-27 22:12:05 +0300

    Task44

---
+++ Task44.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите количество элементов массива:");
+int i = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [i];
+array[1] = 1;
+for (int count = 2; count < i; count++) {
+    array[count] = array[count - 2] + array[count - 1];
+}
+Console.WriteLine(string.Join(" ", array));
